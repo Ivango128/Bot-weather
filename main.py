3fc3ca406bdff3e3cfefdfe00601a204c525290f
Ivan Poplayhin <poplayhin2002@gmail.com>
2024-07-14 14:58:05 +0500

    v3

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import requests
 from dotenv import load_dotenv, find_dotenv
 import os
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 load_dotenv(find_dotenv())
 token = os.getenv('token')
 
 def get_temperature(lat, lon):
     # URL для запроса погоды
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,pressure_msl"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m"
 
     # Отправляем запрос к API
     response = requests.get(url)
@@ -15,8 +18,28 @@ def get_temperature(lat, lon):
     if response.status_code == 200:
         data = response.json()
         # Получаем текущую температуру (последний час в данных)
-        current_temp = str(data['current']['temperature_2m']) + ' Давление' + str(data['current']['pressure_msl'])
-        return current_temp
+        #current_temp = str(data['current']['temperature_2m']) + ' Давление' + str(data['current']['pressure_msl']) + ' Т2 ' + str(data['current']['apparent_temperature'])
+
+        df = pd.DataFrame(data["hourly"])
+
+        # Convert time to datetime
+        df["time"] = pd.to_datetime(df["time"])
+
+        # Plotting the data
+        #plt.figure(figsize=(10, 5))
+        plt.plot(df["time"], df["temperature_2m"], marker='o', linestyle='-', color='b')
+        plt.title('Hourly Temperature Forecast')
+        plt.xlabel('Time')
+        plt.ylabel('Temperature (°C)')
+        plt.grid(True)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        # Save the plot as an image file
+        plt.savefig('hourly_temperature_forecast.png')
+
+
+        return #current_temp
     else:
         print(f"Ошибка: {response.status_code}")
         return None
@@ -43,7 +66,11 @@ def handle_location(message):
     longitude = message.location.longitude
 
     temperature = get_temperature(latitude, longitude)
-    bot.send_message(message.chat.id, f"Спасибо! Погода сейчас {temperature}")
+    bot.send_photo(message.chat.id, open('hourly_temperature_forecast.png', 'rb'))
+    #bot.send_message(message.chat.id, f"Спасибо! Погода сейчас {temperature}")
 
 # Запуск бота
 bot.polling()
+
+
+
